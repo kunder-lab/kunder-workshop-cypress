fe68c73a285dbd218958fa1bc5dd3ee57c7b26ad
Matías <mvergarair@gmail.com>
2019-08-21 21:11:28 -0400

    Added cypress files.

---
+++ cypress/integration/todos.js
@@ -0,0 +1,18 @@
+describe('Todos', function() {
+
+    beforeEach(function () {
+        cy.visit('localhost:8100')
+    })
+
+    it('should navigate to TODO', function() {
+        // Implement Test!
+    })
+
+    it('should update a TODO', function() {
+        // Implement Test!
+    })
+
+    it('should add and delete a todo!', function() {
+        // Implement Test!
+    })
+  })
